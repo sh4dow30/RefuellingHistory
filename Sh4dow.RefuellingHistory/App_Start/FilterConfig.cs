31721a2a87ed88820adb37fb31bee9804823d751
Maciej Skiba <maciej.skiba@microgen.com>
2015-10-14 11:09:07 +0200

    Initial

---
+++ Sh4dow.RefuellingHistory/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Sh4dow.RefuellingHistory
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Sh4dow.RefuellingHistory/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sh4dow.RefuellingHistory.Startup))]
+namespace Sh4dow.RefuellingHistory
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
